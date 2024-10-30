1a926f664bb768af59a5cd6c7c905bcbe72db87f
SeaweedBrain1 <oskar.marzec1234@gmail.com>
2024-10-30 19:56:44 +0100

    Add project files.

---
+++ ObjectInt/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ObjectInt;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] sizes = [1_000_000, 10_000_000, 100_000_000];
+
+        Timer timer = new Timer();
+
+        foreach (int size in sizes)
+        {
+            timer.timerIntObject(size);
+        }
+
+        timer.timerIntObject(sizes);
+    }
+}
---
+++ ObjectInt/Timer.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+
+namespace ObjectInt;
+
+internal class Timer
+{
+    public void timerIntObject(int size)
+    {
+        int[] sourceArray = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++)
+        {
+            sourceArray[i] = random.Next();
+        }
+
+        int[] intArray = new int[size];
+        object[] objectArray = new object[size];
+
+        Stopwatch stopWatchInt = Stopwatch.StartNew();
+        for (int j = 0; j < size; j++)
+        {
+            intArray[j] = sourceArray[j];
+        }
+        stopWatchInt.Stop();
+        long intArrayTime = stopWatchInt.ElapsedMilliseconds;
+        Console.WriteLine($"Time elapsed for int ({size} operations): {intArrayTime} ms");
+
+        Stopwatch stopWatchObject = Stopwatch.StartNew();
+        for (int h = 0; h < size; h++)
+        {
+            objectArray[h] = sourceArray[h];
+        }
+        stopWatchObject.Stop();
+        long objectArrayTime = stopWatchObject.ElapsedMilliseconds;
+        Console.WriteLine($"Time elapsed for object ({size} operations): {objectArrayTime} ms");
+
+        Console.Write('\n');
+    }
+
+    public void timerIntObject(int[] sizes)
+    {
+        foreach (int size in sizes) 
+        {
+            int[] sourceArray = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                sourceArray[i] = random.Next();
+            }
+
+            int[] intArray = new int[size];
+            object[] objectArray = new object[size];
+
+            Stopwatch stopWatchInt = Stopwatch.StartNew();
+            for (int j = 0; j < size; j++)
+            {
+                intArray[j] = sourceArray[j];
+            }
+            stopWatchInt.Stop();
+            long intArrayTime = stopWatchInt.ElapsedMilliseconds;
+            Console.WriteLine($"Time elapsed for int ({size} operations): {intArrayTime} ms");
+
+            Stopwatch stopWatchObject = Stopwatch.StartNew();
+            for (int h = 0; h < size; h++)
+            {
+                objectArray[h] = sourceArray[h];
+            }
+            stopWatchObject.Stop();
+            long objectArrayTime = stopWatchObject.ElapsedMilliseconds;
+            Console.WriteLine($"Time elapsed for object ({size} operations): {objectArrayTime} ms");
+
+            Console.Write('\n');
+        }
+    }
+}
